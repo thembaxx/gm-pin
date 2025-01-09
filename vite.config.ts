723a9c8a7572b09903d74014029790e2bebb933f
Themba <mndebele.themba@gmail.com>
2025-01-10 00:24:29 +0200

    Using a dangerous method for my env, will fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
@@ -17,6 +16,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
       "process.env.GOOGLE_MAPS_API_KEY": JSON.stringify(
         env.GOOGLE_MAPS_API_KEY
       ),
